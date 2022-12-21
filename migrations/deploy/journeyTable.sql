4fda73afe379a5bcf45d62bc83158c0e06ab360a
Salim BENNIS <SBENNIS@absyscyborg.com>
2022-12-21 17:06:09 +0100

    first sqitch deploy

---
+++ migrations/deploy/journeyTable.sql
@@ -0,0 +1,18 @@
+-- Deploy DBwannasurf:journeyTable to pg
+
+BEGIN;
+
+CREATE TABLE "journey" (
+	"id" INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+	"departure_city" TEXT NOT NULL,
+	"destination_surfspot_or_city" TEXT NOT NULL,
+	"meeting_address" TEXT,
+	"departure_time" TIMESTAMP NOT NULL,
+	"price" INT NOT NULL,
+	"place_available" INT NOT NULL,
+	"number_of_boards_allowed" INT NOT NULL,
+	"board_size_allowed" INT NOT NULL,
+	"number_of_boards_loaded" INT	
+);
+
+COMMIT;
