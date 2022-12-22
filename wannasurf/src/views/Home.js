65e053b49a5bc85d8371647f1c9fe90defb0b3c3
François <fplaniol@outlook.fr>
2022-12-22 10:59:21 +0100

    structure home

--- wannasurf/src/views/Home.js
+++ wannasurf/src/views/Home.js
@@ -10,12 +10,13 @@ export const Home = () => {
                 <p>WannaSurf est une application de covoiturage dedié à la communauté de surfer dans le monde</p>
             </div>
 
+            <hr></hr>
             <div className="shearch-bar-section">
-
+                <h2>Barre de recherche</h2>
             </div>
-
+            <hr></hr>
             <div className="bloc-info">
-
+                <h2>Carroussel</h2>
                 <div id="info-bloc-1">
 
 
