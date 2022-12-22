6e680e35b42df9cf7ec9fc8539cb56fe1b706f42
François <fplaniol@outlook.fr>
2022-12-22 16:13:00 +0100

    modif de la Home page et structuration

--- wannasurf/src/views/Home/Home.js
+++ wannasurf/src/views/Home/Home.js
@@ -1,6 +1,10 @@
 import './Home.css';
 import GroupIcon from '@mui/icons-material/Group';
-import { Link } from "react-router-dom"
+import SurfingIcon from '@mui/icons-material/Surfing';
+import { Link } from "react-router-dom";
+import ImageCarrou1 from "../../images/surf-carrou-ex1.jpg";
+import ImageCarrou2 from "../../images/surf-carrou-ex2.jpg";
+import ImageCarrou3 from "../../images/surf-carrou-ex3.jpg";
 
 export const Home = () => {
 
@@ -9,10 +13,13 @@ export const Home = () => {
         <div className="home-bloc">
 
             <div className="intro">
-                <p>WannaSurf est une application de covoiturage dedié à la communauté de surfer dans le monde</p>
+                <p>WannaSurf est une application de covoiturage dedié à la communauté de surfer dans le monde. 
+                    <span>
+                        <SurfingIcon fontSize='small' htmlColor='blue' /> 
+                    </span> 
+                </p>
             </div>
 
-            <hr></hr>
             <div className="shearch-bar-section">
                 <form className="search-form">
                     <dev className="flex-mobile">
@@ -22,47 +29,33 @@ export const Home = () => {
                     <dev className="flex-mobile">
                         <input type="text" placeholder="Date de depart" className="input-search-style" />
                         <GroupIcon fontSize='small' />
-                        <input type="text" placeholder="Nombre de personne" className="input-search-style" />
+                        <input type="range" min="1" max="5" placeholder="Nombre de personne" className="input-search-style" />
                     </dev>
                     <Link to="/wannasurf/trajectsList">
                         <button type="submit" className="btn-search">Trouver un trajet</button>
                     </Link>
                 </form>
             </div>
-            <hr></hr>
+
             <div className="bloc-info">
-                <h2>Carroussel</h2>
-                <div className='texte-corps'>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nunc congue nisi vitae suscipit. Sed risus ultricies tristique nulla aliquet enim tortor. Quam pellentesque nec nam aliquam sem et tortor consequat id. Tempus iaculis urna id volutpat lacus. Id neque aliquam vestibulum morbi blandit cursus risus. Nisi porta lorem mollis aliquam ut porttitor leo a. Aliquam sem et tortor consequat id porta nibh venenatis. Eget mi proin sed libero. Ut porttitor leo a diam sollicitudin.</p>
+                <h2>WannaSurf, ton ami du quotidien !</h2>
 
-                <p>Porta non pulvinar neque laoreet suspendisse interdum. Dapibus ultrices in iaculis nunc sed augue lacus. Ultrices neque ornare aenean euismod elementum. Ac placerat vestibulum lectus mauris ultrices eros in cursus turpis. Nulla at volutpat diam ut venenatis tellus in metus. Risus pretium quam vulputate dignissim suspendisse in est ante. Elit duis tristique sollicitudin nibh sit amet commodo nulla. Tristique sollicitudin nibh sit amet commodo. Quis imperdiet massa tincidunt nunc pulvinar sapien et. Eleifend donec pretium vulputate sapien nec sagittis aliquam malesuada bibendum. Odio tempor orci dapibus ultrices in iaculis. Suscipit tellus mauris a diam maecenas sed enim ut sem. Molestie at elementum eu facilisis sed. Aliquam ut porttitor leo a diam. Eget aliquet nibh praesent tristique magna sit amet. Lectus urna duis convallis convallis. Et molestie ac feugiat sed lectus.</p>
+                    <div id="bloc-info-1">
+                        <h3>Faites des economies pour surfer un max</h3>
+                        <img src={ImageCarrou1} className="img-size" alt="illustration du bloc, faire des economies avec wannaSurf"/>
+                    </div>
+                    <div id="bloc-info-2">
+                        <h3>Pratiquez votre passion tout en respectant l'environnement</h3>
+                        <img src={ImageCarrou2} className="img-size" alt="illustration du bloc, respectez l'environnement avec wannaSurf"/>
+                    </div>
+                    <div id="bloc-info-3">
+                        <h3>Faites vous de nouveaux partenaires de glisse grace à wannaSurf</h3>
+                        <img src={ImageCarrou3} className="img-size close-footer" alt="illustration du bloc, un max de fun avec wannaSurf"/>
+                    </div>
 
-                <p>Euismod in pellentesque massa placerat. Sem nulla pharetra diam sit amet nisl suscipit adipiscing. Consectetur lorem donec massa sapien faucibus et. Placerat vestibulum lectus mauris ultrices eros in cursus. Tellus orci ac auctor augue mauris augue. Quis enim lobortis scelerisque fermentum. Tincidunt id aliquet risus feugiat in. Eget sit amet tellus cras. Vel elit scelerisque mauris pellentesque. Lorem mollis aliquam ut porttitor leo a diam. Dictum fusce ut placerat orci nulla. Justo nec ultrices dui sapien eget mi proin sed libero. Curabitur gravida arcu ac tortor dignissim. Cursus euismod quis viverra nibh cras pulvinar. Nunc non blandit massa enim. Et egestas quis ipsum suspendisse ultrices gravida. Non diam phasellus vestibulum lorem sed risus ultricies tristique. Nunc mi ipsum faucibus vitae aliquet nec ullamcorper. Id velit ut tortor pretium viverra suspendisse potenti. A pellentesque sit amet porttitor eget dolor.</p>
-
-                <p>Pellentesque massa placerat duis ultricies lacus sed. Ac turpis egestas integer eget aliquet. Aliquam sem fringilla ut morbi. Tincidunt dui ut ornare lectus sit amet. Vestibulum lorem sed risus ultricies tristique nulla aliquet enim. Netus et malesuada fames ac turpis. Laoreet suspendisse interdum consectetur libero id. Lectus nulla at volutpat diam ut venenatis. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Vitae sapien pellentesque habitant morbi tristique senectus. Amet luctus venenatis lectus magna. Tristique magna sit amet purus gravida quis blandit turpis cursus. Et pharetra pharetra massa massa ultricies. Vel orci porta non pulvinar. Quisque id diam vel quam elementum pulvinar etiam. Non consectetur a erat nam at lectus urna. Arcu odio ut sem nulla pharetra diam sit amet nisl. Eu sem integer vitae justo eget magna fermentum iaculis eu.</p>
-
-                <p>Sit amet consectetur adipiscing elit duis. Fusce ut placerat orci nulla. Maecenas accumsan lacus vel facilisis volutpat est velit. Mi eget mauris pharetra et ultrices. Fames ac turpis egestas sed tempus urna. Pretium lectus quam id leo in vitae turpis. Fermentum dui faucibus in ornare quam viverra orci. Purus viverra accumsan in nisl nisi scelerisque eu ultrices vitae. Egestas integer eget aliquet nibh praesent tristique. Enim nec dui nunc mattis. Fringilla ut morbi tincidunt augue interdum velit. Consectetur adipiscing elit pellentesque habitant morbi tristique senectus et netus. Ullamcorper eget nulla facilisi etiam.</p>
-
-                <p>Sit amet volutpat consequat mauris nunc. Morbi tristique senectus et netus et malesuada fames. Sed risus ultricies tristique nulla aliquet enim tortor at auctor. Tincidunt dui ut ornare lectus sit amet est. Lectus sit amet est placerat in egestas. Nec tincidunt praesent semper feugiat nibh sed. Eros in cursus turpis massa tincidunt. Augue ut lectus arcu bibendum at varius. Malesuada nunc vel risus commodo viverra maecenas. Elementum eu facilisis sed odio morbi quis commodo odio. Non consectetur a erat nam at. Odio ut sem nulla pharetra diam. Semper risus in hendrerit gravida rutrum quisque. Amet commodo nulla facilisi nullam vehicula ipsum a arcu. Adipiscing elit pellentesque habitant morbi tristique senectus et netus et. Quis hendrerit dolor magna eget est. Tincidunt lobortis feugiat vivamus at augue eget. Id semper risus in hendrerit. Accumsan sit amet nulla facilisi morbi tempus iaculis urna. Vulputate dignissim suspendisse in est ante in nibh.</p>
-
-                <p>Tristique senectus et netus et malesuada. Vitae ultricies leo integer malesuada nunc vel risus commodo. Ipsum a arcu cursus vitae congue mauris rhoncus aenean. Ornare arcu odio ut sem nulla pharetra diam sit amet. Nec ullamcorper sit amet risus nullam eget felis eget nunc. Sit amet nulla facilisi morbi tempus iaculis urna. Vitae turpis massa sed elementum tempus egestas. Vitae auctor eu augue ut lectus arcu bibendum. Feugiat nibh sed pulvinar proin. Ac auctor augue mauris augue neque. Ultricies integer quis auctor elit sed vulputate mi sit. Facilisis gravida neque convallis a cras semper. Vitae nunc sed velit dignissim sodales ut. Risus ultricies tristique nulla aliquet enim tortor at auctor urna. Euismod quis viverra nibh cras. Consequat id porta nibh venenatis cras.</p>
-                
             </div>
-                <div id="info-bloc-1">
 
-
-                </div>
-
-                <div id="info-bloc-2">
-
-                    
-                </div>
-
-                <div id="info-bloc-3">
-
-                
-                </div>
-            </div>
         </div>
+
         )
     }
