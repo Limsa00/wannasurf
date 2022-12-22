a1d962bac3b65ce3ff07cba8ab0f748ae05bf923
Salim BENNIS <SBENNIS@absyscyborg.com>
2022-12-22 12:24:35 +0100

    minor change in architecture

--- index.js
+++ index.js
@@ -1,19 +1,3 @@
-require('dotenv').config();
+const server = require ('./app/server.js');
 
-const express = require('express');
-const router = require('./app/router');
-
-const port = process.env.PORT || 3000;
-const server = express();
-
-// middleware pour avoir accès à req.body
-server.use(express.urlencoded({
-    extended: true,
-}));
-
-// routeur
-server.use(router);
-
-server.listen(port, () => {
-    console.log(`Serveur lancé sur http://localhost:${port}`);
-});
+server.launch();
