82e72365a70f1d30635ed6419531a1db3a6bcfa6
François PLANIOL <FPLANIOL@absyscyborg.com>
2022-12-26 16:50:16 +0100

    add usestate and onchange on home form and setup send fonction

--- Front/src/views/Home/Home.js
+++ Front/src/views/Home/Home.js
@@ -9,58 +9,93 @@ import ImageCarrou3 from "../../images/surf-carrou-ex3.jpg";
 
 export const Home = () => {
     
-    const [nombrePersonne, setNombrePersonne] = useState(1)
+    const [lieuDepart, setLieuDepart] = useState("");
+    const [lieuDestination, setLieuDestination] = useState("");
+    const [dateDepart, setDateDepart] = useState("");
+    const [nombrePersonne, setNombrePersonne] = useState(1);
    
-    return (
+    const envoiFormulaire = (evt) => {
+        evt.preventDefault()
+
+        const trajectSearch = {
+            departure_city: lieuDepart,
+            destination_surfspot_or_city: lieuDestination,
+            departure_time: dateDepart,
+            place_available: nombrePersonne,
+        };
+    }
+
+        return (
     
-        <div className="home-bloc">
+            <div className="home-bloc">
 
-            <div className="intro">
-                <p>WannaSurf est une application de covoiturage dedié à la communauté de surfer dans le monde. 
-                    <span>
-                        <SurfingIcon fontSize='small' htmlColor='blue' /> 
-                    </span> 
-                </p>
-            </div>
+                <div className="intro">
+                    <p>WannaSurf est une application de covoiturage dedié à la communauté de surfer dans le monde.
+                        <span>
+                            <SurfingIcon fontSize='small' htmlColor='blue' />
+                        </span>
+                    </p>
+                </div>
 
-            <div className="shearch-bar-section">
-                <form className="search-form">
-                    <dev className="flex-mobile">
-                        <input type="text" placeholder="Lieu de depart" className="input-search-style" />
-                        <input type="text" placeholder="Lieu de destination" className="input-search-style" />
-                    </dev>
-                    <dev className="flex-mobile">
-                        <input type="text" placeholder="Date de depart" className="input-search-style" />
-                        <GroupIcon fontSize='small' />
-                                <input type="range" min="1" max="8" value={nombrePersonne} onChange={(e) => setNombrePersonne(e.target.value)}  className="input-search-style" />
-                                <output>{nombrePersonne}</output>
+                <div className="shearch-bar-section">
+                    <form className="search-form" onSubmit={envoiFormulaire}>
+                        <dev className="flex-mobile">
+                            <input
+                                type="text"
+                                placeholder="Lieu de depart"
+                                className="input-search-style" 
+                                value={lieuDepart}
+                                onChange={(e) => setLieuDepart(e.target.value)} />
+                            <input
+                                type="text"
+                                placeholder="Lieu de destination"
+                                className="input-search-style" 
+                                value={lieuDestination}
+                                onChange={(e) => setLieuDestination(e.target.value)} />
+                        </dev>
+                        <dev className="flex-mobile">
+                            <input
+                                type="text"
+                                placeholder="Date de depart"
+                                className="input-search-style"
+                                value={dateDepart}
+                                onChange={(e) => setDateDepart(e.target.value)} />
+                            <GroupIcon fontSize='small' />
+                            <input
+                                type="range"
+                                min="1"
+                                max="8"
+                                value={nombrePersonne}
+                                onChange={(e) => setNombrePersonne(e.target.value)}
+                                className="input-search-style" />
+                            <output>{nombrePersonne}</output>
                         
-                    </dev>
-                    <Link to="/wannasurf/trajectsList">
-                        <button type="submit" className="btn-search">Trouver un trajet</button>
-                    </Link>
-                </form>
-            </div>
+                        </dev>
+                        <Link to="/wannasurf/trajectsList">
+                            <button type="submit" className="btn-search">Trouver un trajet</button>
+                        </Link>
+                    </form>
+                </div>
 
-            <div className="bloc-info">
-                <h2>WannaSurf, ton ami du quotidien !</h2>
+                <div className="bloc-info">
+                    <h2>WannaSurf, ton ami du quotidien !</h2>
 
                     <div id="bloc-info-1">
                         <h3>Faites des economies pour surfer un max</h3>
-                        <img src={ImageCarrou1} className="img-size" alt="illustration du bloc, faire des economies avec wannaSurf"/>
+                        <img src={ImageCarrou1} className="img-size" alt="illustration du bloc, faire des economies avec wannaSurf" />
                     </div>
                     <div id="bloc-info-2">
                         <h3>Pratiquez votre passion tout en respectant l'environnement</h3>
-                        <img src={ImageCarrou2} className="img-size" alt="illustration du bloc, respectez l'environnement avec wannaSurf"/>
+                        <img src={ImageCarrou2} className="img-size" alt="illustration du bloc, respectez l'environnement avec wannaSurf" />
                     </div>
                     <div id="bloc-info-3">
                         <h3>Faites vous de nouveaux partenaires de glisse grace à wannaSurf</h3>
-                        <img src={ImageCarrou3} className="img-size close-footer" alt="illustration du bloc, un max de fun avec wannaSurf"/>
+                        <img src={ImageCarrou3} className="img-size close-footer" alt="illustration du bloc, un max de fun avec wannaSurf" />
                     </div>
 
-            </div>
+                </div>
 
-        </div>
+            </div>
 
         )
     }
