8d264d8a811dafc00cd42d379a2421378e819217
François <fplaniol@outlook.fr>
2023-01-18 13:27:41 +0100

    minor change

--- Front/src/components/Footer/Footer.js
+++ Front/src/components/Footer/Footer.js
@@ -5,10 +5,18 @@ export const Footer = () => {
     return (
         <div id="footer" className="footer-bloc">
             <div className="left-footer">
-                <Link to="/wannasurf/legalNotice" className="link-style">Informations légales</Link>
+                <Link 
+                    to="/wannasurf/legalNotice" 
+                    className="link-style">
+                        Informations légales
+                </Link>
             </div>
             <div className="right-footer">
-                <Link to="/wannasurf/siteContact" className="link-style">Nous contacter</Link>
+                <Link 
+                    to="/wannasurf/siteContact" 
+                    className="link-style">
+                        Nous contacter
+                </Link>
             </div>
         </div>
     )
