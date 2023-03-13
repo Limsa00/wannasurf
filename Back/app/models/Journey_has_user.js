8cacc994ac576f151862c0a5c62a2355964ca5a5
Salim BENNIS <SBENNIS@absyscyborg.com>
2023-03-13 16:09:01 +0100

    userJourneys & journeyUsers routes OK

--- Back/app/models/Journey_has_user.js
+++ Back/app/models/Journey_has_user.js
@@ -11,6 +11,16 @@ class Journey_has_user extends CoreModel {
             this[prop] = data[prop];
         }
     }
+
+    static async findOneJourneyUsers(journeyId) {
+        const oneJourney_has_user = await db.query (`SELECT * FROM journey_has_user WHERE journey_id=$1;`, [journeyId]);
+        return oneJourney_has_user.rows;
+    }
+
+    static async findOneUserJourneys (userId) {
+        const oneUser_has_journey = await db.query (`SELECT * FROM journey_has_user WHERE user_id=$1;`, [userId]);
+        return oneUser_has_journey.rows;
+    }
 }
 
 module.exports = Journey_has_user;
--- Back/app/router.js
+++ Back/app/router.js
@@ -2,6 +2,8 @@ const express = require('express');
 const journeyController = require('./controllers/journeyController');
 const loginController = require('./controllers/loginController');
 const mainController = require('./controllers/mainController');
+const journey_has_userController = require('./controllers/journey_has_userController');
+
 const authMW = require('./services/authMW')
 
 
@@ -12,6 +14,8 @@ router.get('/journeySearch', journeyController.showJourneysFiltered);
 router.get('/user/:uid', loginController.findUserWithUid);
 router.patch('/user/:uid', loginController.editUser);
 
+router.get('/journey_has_user/:id', journey_has_userController.showOneJourneyUsers);
+router.get('/user_has_journey/:id', journey_has_userController.showOneUserJourneys);
 
 // router.get('/journeys', journeyController.showAllJourneys);
 // router.get('/journeys/:id', journeyController.showOneJourney);
@@ -27,7 +31,7 @@ router.patch('/user/:uid', loginController.editUser);
 // Factoring routes for models : journey, user
 router.get('/:entity', mainController.showAllComponents);
 router.get('/:entity/:id', mainController.showOneComponent);
-router.post('/:entity', authMW, mainController.addOneComponent);
+router.post('/:entity', /*authMW,*/ mainController.addOneComponent);
 router.patch('/:entity/:id', mainController.editOneComponent);
 router.delete('/:entity/:id', mainController.deleteOneComponent);
 
