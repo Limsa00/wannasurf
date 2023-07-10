e8bd9fdfdfb9f6d57c86a32272a83d655e28f22e
franswap <fplaniol@outlook.fr>
2023-07-10 12:17:09 +0200

    swagger doc

--- Back/app/router.js
+++ Back/app/router.js
@@ -1,4 +1,7 @@
 const express = require('express');
+const swaggerJsDoc = require('swagger-jsdoc');
+const swaggerUI = require('swagger-ui-express');
+
 const journeyController = require('./controllers/journeyController');
 const userController = require('./controllers/userController');
 const mainController = require('./controllers/mainController')
@@ -13,6 +16,357 @@ const authMW = require('./services/authMW')
 const entityToUseControl = require('./services/getEntityToUse');
 
 const router = express.Router();
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocs));
+
+/**
+ * @swagger
+ * tags:
+ *   name: Journey
+ *   description: API routes for managing journeys
+ */
+
+/**
+ * @swagger
+ * /journeySearch:
+ *   get:
+ *     summary: Search for journeys based on date and available seats
+ *     tags: [Journey]
+ *     parameters:
+ *       - in: query
+ *         name: date
+ *         required: true
+ *         schema:
+ *           type: string
+ *           format: date
+ *       - in: query
+ *         name: seats
+ *         required: true
+ *         schema:
+ *           type: integer
+ *           minimum: 1
+ *     responses:
+ *       200:
+ *         description: Returns the filtered list of journeys
+ */
+
+/**
+ * @swagger
+ * /myTravels/{id}:
+ *   get:
+ *     summary: Get detailed information about a user's journeys
+ *     tags: [Journey]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Returns detailed information about the user's journeys
+ */
+
+/**
+ * @swagger
+ * /userId/{id}:
+ *   get:
+ *     summary: Find a user by ID
+ *     tags: [User]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Returns the user with the specified ID
+ */
+
+/**
+ * @swagger
+ * /userUid/{uid}:
+ *   get:
+ *     summary: Find a user by UID
+ *     tags: [User]
+ *     parameters:
+ *       - in: path
+ *         name: uid
+ *         required: true
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Returns the user with the specified UID
+ */
+
+/**
+ * @swagger
+ * /user/{uid}:
+ *   patch:
+ *     summary: Edit a user's information
+ *     tags: [User]
+ *     parameters:
+ *       - in: path
+ *         name: uid
+ *         required: true
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Returns the updated user information
+ */
+
+/**
+ * @swagger
+ * /user/deactivate/{id}:
+ *   patch:
+ *     summary: Edit a user's information for removal
+ *     tags: [User]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Returns the updated user information
+ */
+
+/**
+ * @swagger
+ * /journey_has_user/{id}:
+ *   get:
+ *     summary: Get the list of passengers in a journey
+ *     tags: [JourneyHasUser]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Returns the list of passengers in the journey
+ */
+
+/**
+ * @swagger
+ * /user_has_journey/{id}:
+ *   get:
+ *     summary: Get the list of journeys a user is registered in
+ *     tags: [JourneyHasUser]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Returns the list of journeys for the user
+ */
+
+/**
+ * @swagger
+ * /journey_has_user/{journeyId}/{userId}:
+ *   get:
+ *     summary: Get a specific journey and passenger
+ *     tags: [JourneyHasUser]
+ *     parameters:
+ *       - in: path
+ *         name: journeyId
+ *         required: true
+ *         schema:
+ *           type: string
+ *       - in: path
+ *         name: userId
+ *         required: true
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Returns the specified journey and passenger
+ */
+
+/**
+ * @swagger
+ * /journey_has_user:
+ *   post:
+ *     summary: Add a user to a journey
+ *     tags: [JourneyHasUser]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/journeyHasUserSchema'
+ *     responses:
+ *       200:
+ *         description: Returns the added user and journey information
+ */
+
+/**
+ * @swagger
+ * /journey_has_user/{journeyId}/{userId}:
+ *   delete:
+ *     summary: Remove a user from a journey
+ *     tags: [JourneyHasUser]
+ *     parameters:
+ *       - in: path
+ *         name: journeyId
+ *         required: true
+ *         schema:
+ *           type: string
+ *       - in: path
+ *         name: userId
+ *         required: true
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Returns a success message
+ */
+
+/**
+ * @swagger
+ * /journey:
+ *   post:
+ *     summary: Create a new journey
+ *     tags: [Journey]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/newJourneySchema'
+ *     responses:
+ *       200:
+ *         description: Returns the created journey information
+ */
+
+/**
+ * @swagger
+ * /{entity}:
+ *   get:
+ *     summary: Get all components of a specific entity
+ *     tags: [Entity]
+ *     parameters:
+ *       - in: path
+ *         name: entity
+ *         required: true
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Returns the list of components for the specified entity
+ */
+
+/**
+ * @swagger
+ * /{entity}/{id}:
+ *   get:
+ *     summary: Get a specific component of an entity
+ *     tags: [Entity]
+ *     parameters:
+ *       - in: path
+ *         name: entity
+ *         required: true
+ *         schema:
+ *           type: string
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Returns the specified component
+ */
+
+/**
+ * @swagger
+ * /{entity}:
+ *   post:
+ *     summary: Add a new component to an entity
+ *     tags: [Entity]
+ *     parameters:
+ *```javascript
+/**
+ * @swagger
+ * /{entity}:
+ *   post:
+ *     summary: Add a new component to an entity
+ *     tags: [Entity]
+ *     parameters:
+ *       - in: path
+ *         name: entity
+ *         required: true
+ *         schema:
+ *           type: string
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/componentSchema'  // Replace "componentSchema" with the actual schema for your component
+ *     responses:
+ *       200:
+ *         description: Returns the added component
+ */
+
+/**
+ * @swagger
+ * /{entity}/{id}:
+ *   patch:
+ *     summary: Edit a specific component of an entity
+ *     tags: [Entity]
+ *     parameters:
+ *       - in: path
+ *         name: entity
+ *         required: true
+ *         schema:
+ *           type: string
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/componentSchema'  // Replace "componentSchema" with the actual schema for your component
+ *     responses:
+ *       200:
+ *         description: Returns the updated component
+ */
+
+/**
+ * @swagger
+ * /{entity}/{id}:
+ *   delete:
+ *     summary: Delete a specific component of an entity
+ *     tags: [Entity]
+ *     parameters:
+ *       - in: path
+ *         name: entity
+ *         required: true
+ *         schema:
+ *           type: string
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Returns a success message
+ */
 
 // Rechercher les trajets à partir de la date et des places dispos
 router.get('/journeySearch', validateQuery(journeySearchSchema),journeyController.showJourneysFiltered);
